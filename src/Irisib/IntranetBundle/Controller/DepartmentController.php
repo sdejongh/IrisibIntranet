29cae2b4c0e4f59bb2ab50e29278330d7c932f45
Steve De Jongh <dejongh.st@gmail.com>
2015-06-05 15:15:24 +0200

    Added Department suppression dialog Stated adding new Department form

--- src/Irisib/IntranetBundle/Controller/DepartmentController.php
+++ src/Irisib/IntranetBundle/Controller/DepartmentController.php
@@ -2,7 +2,10 @@
 
 namespace Irisib\IntranetBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use Irisib\IntranetBundle\Entity\Department;
 
 class DepartmentController extends Controller
 {
@@ -14,4 +17,102 @@ class DepartmentController extends Controller
         $departments = $this->getDoctrine()->getRepository('IrisibIntranetBundle:Department')->findAll();
         return $this->render('IrisibIntranetBundle:Department:listDepartment.html.twig',array('departments' => $departments));
     }
+
+    /***********************
+     * DEPARTMENT CREATION *
+     ***********************/
+
+    public function createAction(Request $request)
+    {
+        $department = new Department();
+        $form = $this->createNewForm($department);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($department);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('department_list'));
+        }
+
+        return $this->render('IrisibIntranetBundle:Department:formNewDepartment.html.twig', array(
+            'department' => $department,
+            'form'   => $form->createView(),
+        ));
+    }
+
+    private function createNewForm(Department $department)
+    {
+        $form = $this->createForm(new Department(), $department, array(
+            'action' => $this->generateUrl('department_create'),
+            'method' => 'POST',
+        ));
+
+        return $form;
+    }
+
+
+    public function newAction()
+    {
+        $department = new Department();
+        $form   = $this->createNewForm($department);
+
+        return $this->render('IrisibIntranetBundle:Department:formNewDepartment.html.twig', array(
+            'entity' => $department,
+            'form'   => $form->createView(),
+        ));
+    }
+
+
+
+    /********************
+     * DELETE FUNCTIONS *
+     ********************/
+    public function deleteAction(Request $request, $id)
+    {
+        $form = $this->createDeleteForm($id);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $entity = $em->getRepository('IrisibIntranetBundle:Department')->find($id);
+
+            if (!$entity) {
+                throw $this->createNotFoundException('Unable to find Department entity.');
+            }
+
+            $em->remove($entity);
+            $em->flush();
+
+        }
+        return $this->redirect($this->generateUrl('department_list'));
+
+    }
+
+    public function createDeleteForm($id)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('department_delete', array('id' => $id)))
+            ->setMethod('DELETE')
+            ->getForm()
+            ;
+    }
+
+    public function deleteFormAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $department = $em->getRepository('IrisibIntranetBundle:Department')->find($id);
+        $deleteForm = $this->createDeleteForm($id);
+
+        return $this->render('IrisibIntranetBundle:Department:formDeleteDepartment.html.twig',
+            array(
+                'delete_form' => $deleteForm->createView(),
+                'id' => $id,
+                'name' => $department->getFullname(),
+
+            )
+        );
+    }
+
 }
